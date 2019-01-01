9aaf695baabba96ef0cf438a10dca6620c15dda5
anseny <qtkiller@163.com>
2019-01-01 23:55:09 +0800

    Create x.js

---
+++ js/x.js
@@ -0,0 +1,41 @@
+var headervar = document.getElementById("header");
+var container = document.getElementById("imgs");
+var index = 0;
+var imgs = container.getElementsByTagName("img");
+var timer = null;
+var titles = document.getElementById("title");
+var ts = titles.getElementsByTagName("div"); 
+window.onload = function imgshow(){
+    header.onmouseover = function(){
+        clearInterval(timer);
+    }
+    header.onmouseout = function(){
+        timer = setInterval(function(){
+            index++;  
+            if(index>3){
+                index = 0;
+            } 
+            //console.log(index); 
+            changeimg(index);
+        },2000)
+    }
+    for(var j = 0;j<ts.length;j++){
+        ts[j].id = j;
+        ts.style.background = "white";
+        ts[j].onclick = function(){
+            index = this.id;
+            ts.style.background = "white";
+            changeimg(index);
+
+        }
+    }
+
+}
+function changeimg(index){
+
+    for(var i = 0;i<imgs.length;i++){
+        imgs[i].style.display = "none";
+    }
+    imgs[index].style.display = "block";
+
+}
